788685f3f84d7bd2fe0377d8c8c517c0275d853e
Kushagra Singh <kushthaksh@gmail.com>
2021-04-17 22:24:07 -0700

    Dashboard Frontend Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sharkstonks/Views/Dashboard.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,9 +21,10 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
+        backgroundColor: Colors.black,
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: DashboardView(),
     );
   }
 }
